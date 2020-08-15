02927cc070eb00430c468e1e628c8a6ddead371d
Ritika-soni <ritikasoni73@gmail.com>
2020-08-14 18:33:02 -0700

    Updates

---
+++ precache-manifest.82924fd701cab57d2dde97f90f3a5fbc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82a27a8bd7a9758b32e547cca180655",
+    "url": "/PanorbitAssignment1/index.html"
+  },
+  {
+    "revision": "ae3722f77118d0a11346",
+    "url": "/PanorbitAssignment1/static/css/2.8462b5f9.chunk.css"
+  },
+  {
+    "revision": "3f8def3a35c0299c9557",
+    "url": "/PanorbitAssignment1/static/css/main.60569740.chunk.css"
+  },
+  {
+    "revision": "ae3722f77118d0a11346",
+    "url": "/PanorbitAssignment1/static/js/2.9ebae81e.chunk.js"
+  },
+  {
+    "revision": "d37f3357be2991bc6571123267ce5223",
+    "url": "/PanorbitAssignment1/static/js/2.9ebae81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8def3a35c0299c9557",
+    "url": "/PanorbitAssignment1/static/js/main.06d51c1f.chunk.js"
+  },
+  {
+    "revision": "5080b9461bad0e629451",
+    "url": "/PanorbitAssignment1/static/js/runtime-main.f56bc6d2.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/PanorbitAssignment1/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/PanorbitAssignment1/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/PanorbitAssignment1/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/PanorbitAssignment1/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PanorbitAssignment1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/PanorbitAssignment1/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/PanorbitAssignment1/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/PanorbitAssignment1/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/PanorbitAssignment1/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/PanorbitAssignment1/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
